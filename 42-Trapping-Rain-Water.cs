911fe9913652f305f7bbe260fe4fc989ef130242
Abdulaziz Toba <122528370+AbdulazeezToba@users.noreply.github.com>
2025-02-17 13:05:44 +0200

    [42] [Time Beats: 100.00%] [Memory Beats: 67.64%] - LeetPush

---
+++ 42-Trapping-Rain-Water.cs
@@ -0,0 +1,28 @@
+public class Solution {
+    public int Trap(int[] height) {
+        int n = height.Length;
+        if (n == 0) return 0;
+
+        int left = 0, right = n - 1, leftMax = 0, rightMax = 0, water = 0;
+
+        while (left <= right) {
+            if (height[left] <= height[right]) {
+                if (height[left] >= leftMax) {
+                    leftMax = height[left];
+                } else {
+                    water += leftMax - height[left];
+                }
+                left++;
+            } else {
+                if (height[right] >= rightMax) {
+                    rightMax = height[right];
+                } else {
+                    water += rightMax - height[right];
+                }
+                right--;
+            }
+        }
+
+        return water;
+    }
+}
